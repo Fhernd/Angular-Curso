abb74e1d14051def38d745d85e8aa4e33cfc6188
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-07 07:18:33 -0500

    Interpolación de variables con datos del modelo

--- proyecto001-prueba/src/app/app.component.ts
+++ proyecto001-prueba/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto001-prueba';
+  nombre = 'John Ortiz Ordoñez';
+  edad = 29;
+  email = 'joo@mail.co';
+  sueldos = [2000, 3000, 5000];
+  activo = true;
+
+  esActivo() {
+    return this.activo ? 'Trabajador activo' : 'Trabajador inactivo';
+  }
+
+  ultimos3Sueldos() {
+    return this.sueldos.reduce((a, v) => (a += v, a), 0);
+  }
 }

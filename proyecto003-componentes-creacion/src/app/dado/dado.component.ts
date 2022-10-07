310c0d50099e3effca386364e4c6c895a98cb1a3
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-07 15:36:50 -0500

    La función ngOnInit() inicializa una variable con un valor dado

--- proyecto003-componentes-creacion/src/app/dado/dado.component.ts
+++ proyecto003-componentes-creacion/src/app/dado/dado.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dado.component.css']
 })
 export class DadoComponent implements OnInit {
+  valor: number = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.valor = Math.trunc(Math.random() * 6) + 1;
   }
 
 }

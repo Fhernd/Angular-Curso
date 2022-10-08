9b2a218a9065c1af3b42a71313ce456e90b80994
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-08 14:08:19 -0500

    Métodos para incrementar, decrementar y fijar valores del componente

--- proyecto006-metodos-hijo/src/app/selector-numerico/selector-numerico.component.ts
+++ proyecto006-metodos-hijo/src/app/selector-numerico/selector-numerico.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-selector-numerico',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorNumericoComponent implements OnInit {
 
+  @Input()
+  minimo: number = 1;
+  @Input()
+  maximo: number = 1;
+  actual: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.actual = this.minimo;
   }
 
+  incrementar() {
+    if (this.actual < this.maximo) {
+      ++this.actual;
+    }
+  }
+
+  decrementar() {
+    if (this.actual > this.minimo) {
+      --this.actual;
+    }
+  }
+
+  fijar(valor: number) {
+    if (valor >= this.minimo && valor <= this.maximo) {
+      this.actual = valor;
+    }
+  }
 }

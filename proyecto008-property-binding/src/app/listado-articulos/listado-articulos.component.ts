e5631e39ad452e25868ccd1f09efbae6f4e4a549
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-08 16:55:44 -0500

    Atributo para entrada de datos (artículos)

--- proyecto008-property-binding/src/app/listado-articulos/listado-articulos.component.ts
+++ proyecto008-property-binding/src/app/listado-articulos/listado-articulos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listado-articulos',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado-articulos.component.css']
 })
 export class ListadoArticulosComponent implements OnInit {
+
+  @Input()
+  articulos: any;
 
   constructor() { }
 

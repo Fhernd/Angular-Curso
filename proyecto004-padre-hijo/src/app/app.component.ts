29877ea645de9fc94aa1eec021c10bd43d1b0cd1
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-08 08:36:17 -0500

    Lógica para tirar o lanzar los tres dados

--- proyecto004-padre-hijo/src/app/app.component.ts
+++ proyecto004-padre-hijo/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   valor1: number;
   valor2: number;
   valor3: number;
+  resultado: string = '';
 
   constructor() {
     this.valor1 = this.generarAleatorio();
@@ -17,5 +18,17 @@ export class AppComponent {
   }
   generarAleatorio(): number {
     return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  tirar() {
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3) {
+      this.resultado = 'Ganó';
+    } else {
+      this.resultado = 'Perdió';
+    }
   }
 }

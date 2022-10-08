48538c8066f2c9ea3ca507e6bcc195cc48751dad
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-08 08:34:16 -0500

    Definida la interfaz para lanzar dados

--- proyecto004-padre-hijo/src/app/app.component.ts
+++ proyecto004-padre-hijo/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto004-padre-hijo';
+  valor1: number;
+  valor2: number;
+  valor3: number;
+
+  constructor() {
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+  }
+  generarAleatorio(): number {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
 }

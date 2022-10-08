a0cfddeeb2bf529cd708294dbb873dae5d519494
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-08 09:06:32 -0500

    Creada la estructura para la componente de Cronómetro

--- proyecto005-disparo-eventos/src/app/app.module.ts
+++ proyecto005-disparo-eventos/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CronometroComponent } from './cronometro/cronometro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CronometroComponent
   ],
   imports: [
     BrowserModule
---
+++ proyecto005-disparo-eventos/src/app/cronometro/cronometro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cronometro',
+  templateUrl: './cronometro.component.html',
+  styleUrls: ['./cronometro.component.css']
+})
+export class CronometroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

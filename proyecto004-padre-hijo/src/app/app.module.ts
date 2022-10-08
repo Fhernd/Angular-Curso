ddf63a44b3c46f03dbc249a11fcbdbd69157eede
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-08 07:44:24 -0500

    Creada la componente DadoComponent

--- proyecto004-padre-hijo/src/app/app.module.ts
+++ proyecto004-padre-hijo/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DadoComponent } from './dado/dado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadoComponent
   ],
   imports: [
     BrowserModule

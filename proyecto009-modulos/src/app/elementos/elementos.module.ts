dcbdba310f68592c2d52bed7f1b5c56e8c06fe24
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-10 16:12:20 -0500

    Componente Dado en el módulo elementos

--- proyecto009-modulos/src/app/elementos/elementos.module.ts
+++ proyecto009-modulos/src/app/elementos/elementos.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DadoComponent } from './dado/dado.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DadoComponent
+  ],
   imports: [
     CommonModule
   ]

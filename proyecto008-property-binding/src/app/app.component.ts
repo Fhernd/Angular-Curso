f8bfa27fd945bd9701769d1c93028c2f4dd0888f
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-10 07:18:38 -0500

    Listado fijo de artículos

--- proyecto008-property-binding/src/app/app.component.ts
+++ proyecto008-property-binding/src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto008-property-binding';
+  articulos = [
+    {
+      codigo: 1,
+      descripcion: 'papas',
+      precio: 10.55
+    },
+    {
+      codigo: 2,
+      descripcion: 'manzanas',
+      precio: 12.10
+    },
+    {
+      codigo: 3,
+      descripcion: 'melon',
+      precio: 52.30
+    },
+    {
+      codigo: 4,
+      descripcion: 'cebollas',
+      precio: 17
+    },
+    {
+      codigo: 5,
+      descripcion: 'calabaza',
+      precio: 20
+    }
+  ];
 }

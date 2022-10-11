35d6ee074455721f2e86f85ad52af9ce161ab6e4
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-11 16:08:37 -0500

    Estructura inicial para la componente Dado

---
+++ proyecto011-rutas/src/app/juego-dados/juego-dados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juego-dados',
+  templateUrl: './juego-dados.component.html',
+  styleUrls: ['./juego-dados.component.css']
+})
+export class JuegoDadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

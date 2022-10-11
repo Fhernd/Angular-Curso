f117cd575cf1d1a1a562c6898368f8ef9ed992c7
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-11 13:30:39 -0500

    Lógica para traer datos desde un servidor remoto

--- proyecto010-json/src/app/app.component.ts
+++ proyecto010-json/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto010-json';
+export class AppComponent implements OnInit {
+  articulos: any;
+    
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://scratchya.com.ar/vue/datos.php').subscribe(resultado => {
+      this.articulos = resultado;
+    });
+  }
 }

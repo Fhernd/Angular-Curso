640220af5c7b92307ca7e2481c44ed119a92fb00
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-11 16:15:54 -0500

    Estructura inicial para la componente DadoComponent

--- proyecto011-rutas/src/app/app.module.ts
+++ proyecto011-rutas/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JuegoDadosComponent } from './juego-dados/juego-dados.component';
+import { DadoComponent } from './dado/dado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JuegoDadosComponent
+    JuegoDadosComponent,
+    DadoComponent
   ],
   imports: [
     BrowserModule,

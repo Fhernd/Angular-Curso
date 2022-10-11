08451cffbebfcf4f864ba3ba51c41f25a5537d1f
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-11 16:28:21 -0500

    Agregación de rutas para navegación

--- proyecto011-rutas/src/app/app-routing.module.ts
+++ proyecto011-rutas/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { JuegoDadosComponent } from './juego-dados/juego-dados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'juego-dados', component: JuegoDadosComponent},
+  {path: 'acerca-de', component: AcercaDeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

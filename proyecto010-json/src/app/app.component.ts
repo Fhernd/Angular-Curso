d191aefa62ae880f831b64309f29100bd93e27df
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-11 13:51:23 -0500

    Solicitud de datos a la API de COVID-19

--- proyecto010-json/src/app/app.component.ts
+++ proyecto010-json/src/app/app.component.ts
@@ -8,12 +8,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   articulos: any;
+  paises: any;
     
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get('https://scratchya.com.ar/vue/datos.php').subscribe(resultado => {
       this.articulos = resultado;
+    });
+
+    this.http.get('https://api.covid19api.com/summary').subscribe(resultado => {
+      this.paises = resultado;
     });
   }
 }

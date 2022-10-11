64262b62d3751c2b9a838a171879c4cf4e929b8f
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-11 16:11:07 -0500

    Lógica de la componente Dado.

--- proyecto011-rutas/src/app/juego-dados/juego-dados.component.ts
+++ proyecto011-rutas/src/app/juego-dados/juego-dados.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./juego-dados.component.css']
 })
 export class JuegoDadosComponent implements OnInit {
+  valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado: string = '';
 
-  constructor() { }
+  constructor() {
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+   }
 
   ngOnInit(): void {
   }
 
+  generarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  tirar() {
+    this.valor1 = this.generarAleatorio();
+    this.valor2 = this.generarAleatorio();
+    this.valor3 = this.generarAleatorio();
+
+    if (this.valor1 == this.valor2 && this.valor1 == this.valor3) {
+      this.resultado = 'Ganó';
+    } else {
+      this.resultado = 'Perdió';
+    }
+  }
 }

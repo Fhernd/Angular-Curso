1d997f611beb5e8c4eb54d773a14c130b970e9db
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-12 16:35:33 -0500

    Implementada la función transform()

--- proyecto015-pipes-personalizadas/src/app/pipes/letras.pipe.ts
+++ proyecto015-pipes-personalizadas/src/app/pipes/letras.pipe.ts
@@ -5,7 +5,62 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class LetrasPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: any, ...args: any[]): any {
+    if (args.length) {
+      if (args[0] == 'inglés') {
+        switch (value) {
+          case 1:
+            return 'one';
+          case 2:
+            return 'two';
+          case 3:
+            return 'three';
+          case 4:
+            return 'four';
+          case 5:
+            return 'five';
+          case 6:
+            return 'six';
+          case 7:
+            return 'six';
+        }
+      } else if (args[0] == 'portugués') {
+        switch (value) {
+          case 1:
+            return 'um';
+          case 2:
+            return 'dois';
+          case 3:
+            return 'três';
+          case 4:
+            return 'quatro';
+          case 5:
+            return 'cinco';
+          case 6:
+            return 'seis';
+          case 7:
+            return 'sete';
+        }
+      } else {
+        switch (value) {
+          case 1:
+            return 'uno';
+          case 2:
+            return 'dos';
+          case 3:
+            return 'tres';
+          case 4:
+            return 'cuatro';
+          case 5:
+            return 'cinco';
+          case 6:
+            return 'seis';
+          case 7:
+            return 'siete';
+        }
+      }
+    }
+
     return null;
   }
 

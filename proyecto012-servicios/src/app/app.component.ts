1995fd56f092c9b9a4d4eb82e6e6768761975440
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-12 08:10:15 -0500

    Lógica para obtener datos de artículos en el modelo

--- proyecto012-servicios/src/app/app.component.ts
+++ proyecto012-servicios/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticulosService } from './services/articulos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto012-servicios';
+export class AppComponent implements OnInit {
+  articulos: any;
+
+  constructor(private articulosServicio: ArticulosService) {
+
+  }
+
+  ngOnInit(): void {
+      this.articulos = this.articulosServicio.obtenerArticulos();
+  }
 }

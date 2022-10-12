533f754ae415a40e4966980b820d90be4116dc00
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-12 13:34:35 -0500

    Método para obtener artículos desde una solicitud GET

--- proyecto013-servicios-web/src/app/services/articulos.service.ts
+++ proyecto013-servicios-web/src/app/services/articulos.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class ArticulosService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  obtenerArticulos() {
+    return this.http.get('https://scratchya.com.ar/vue/datos.php');
+  }
 }

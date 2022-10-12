75ae74989f3cf3242373ce98816581b7311317f0
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-12 13:39:47 -0500

    Leer los datos desde el modelo de AppComponent

--- proyecto013-servicios-web/src/app/app.component.ts
+++ proyecto013-servicios-web/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticulosService } from './services/articulos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto013-servicios-web';
+export class AppComponent implements OnInit {
+
+  articulos: any;
+
+  constructor(private articulosService: ArticulosService) {
+
+  }
+  
+  ngOnInit(): void {
+      this.articulosService.obtenerArticulos().subscribe((resultado) => {
+        this.articulos = resultado;
+      });
+  }
 }

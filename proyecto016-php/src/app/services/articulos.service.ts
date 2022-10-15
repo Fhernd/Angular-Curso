ce0e8b5f2740be4e774d3dc9f320995dd0c4fb4c
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-15 13:18:42 -0500

    Incorporado módulo HttpClientModule

--- proyecto016-php/src/app/services/articulos.service.ts
+++ proyecto016-php/src/app/services/articulos.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticulosService {
+  url: string = 'https://scratchya.com.ar/angular/proyecto016/';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 }

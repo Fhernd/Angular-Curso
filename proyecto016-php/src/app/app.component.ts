1b8a74b2e74c62d3ebb5a502ef4b0c3eed3587d3
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-17 08:04:18 -0500

    Empezando con el componente principal

--- proyecto016-php/src/app/app.component.ts
+++ proyecto016-php/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticulosService } from './services/articulos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto016-php';
+export class AppComponent implements OnInit {
+  articulos: any;
+
+  articulo = {
+    codigo: 0,
+    descripcion: '', 
+    precio: 0
+  }
+
+  constructor(private articulosService: ArticulosService) {
+
+  }
+
+  ngOnInit(): void {
+      this.obtenerArticulos();
+  }
+
+  obtenerArticulos() {
+    this.articulosService.obtenerArticulos().subscribe((resultado) => {
+      this.articulos = resultado;
+    })
+  }
 }

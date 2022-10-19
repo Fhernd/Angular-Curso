c9f0fe527f9655c974720bf8befca144d91b83e0
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-19 08:25:14 -0500

    Proyecto 17 - Prueba de ng new

---
+++ proyecto017/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'joo-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'proyecto017';
+}

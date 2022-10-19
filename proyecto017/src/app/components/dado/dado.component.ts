6447988b0f8683f7eb4093f61560dd167fd5c95a
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-19 08:32:40 -0500

    Creada una componente con prefijo personalizado

---
+++ proyecto017/src/app/components/dado/dado.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'juego-dado',
+  templateUrl: './dado.component.html',
+  styles: [
+  ]
+})
+export class DadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

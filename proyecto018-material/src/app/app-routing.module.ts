d45be18ce531e82d81b47030eff5a2b95d6936cd
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-19 16:51:01 -0500

    Agregadas las rutas para los países

--- proyecto018-material/src/app/app-routing.module.ts
+++ proyecto018-material/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaisArgentinaComponent } from './components/pais-argentina/pais-argentina.component';
+import { PaisColombiaComponent } from './components/pais-colombia/pais-colombia.component';
+import { PaisMexicoComponent } from './components/pais-mexico/pais-mexico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pais-colombia', component: PaisColombiaComponent},
+  {path: 'pais-argentina', component: PaisArgentinaComponent},
+  {path: 'pais-mexico', component: PaisMexicoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

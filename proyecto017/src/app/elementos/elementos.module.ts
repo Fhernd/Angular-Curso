3195c0afffacb47bd46a5249edb7542498bb8b92
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-19 08:44:48 -0500

    Creada la componente Tablero en el módulo elementos

--- proyecto017/src/app/elementos/elementos.module.ts
+++ proyecto017/src/app/elementos/elementos.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableroComponent } from './tablero/tablero.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableroComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ proyecto017/src/app/elementos/tablero/tablero.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'joo-tablero',
+  templateUrl: './tablero.component.html',
+  styles: [
+  ]
+})
+export class TableroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

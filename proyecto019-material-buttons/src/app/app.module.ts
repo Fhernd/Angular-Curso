f866dd3143422bc11c6031e86567da022e860d71
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-20 11:44:13 -0500

    Módulo incorporado para botones de Material

--- proyecto019-material-buttons/src/app/app.module.ts
+++ proyecto019-material-buttons/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

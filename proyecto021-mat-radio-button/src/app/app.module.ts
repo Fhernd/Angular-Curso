41abfefd18f1987072a1f51be5184e8df858dd11
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-21 16:23:37 -0500

    Módulos Material importados al proyecto

--- proyecto021-mat-radio-button/src/app/app.module.ts
+++ proyecto021-mat-radio-button/src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    FormsModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatRadioModule, 
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]

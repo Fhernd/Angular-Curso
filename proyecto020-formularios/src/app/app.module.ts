5eed4a393083e0557afd47398f2de3d2dd5c6ff4
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-21 13:36:25 -0500

    Módulos para entrada y botones de Angular Material

--- proyecto020-formularios/src/app/app.module.ts
+++ proyecto020-formularios/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    FormsModule, 
+    MatInputModule, 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

59727cc131db503ad741525e6776b3bf3daa7ee8
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-22 14:27:28 -0500

    Lógica de la calculadora en un componente

--- proyecto023-mat-select/src/app/app.component.ts
+++ proyecto023-mat-select/src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto023-mat-select';
+  valor1!: number;
+  valor2!: number;
+  resultado!:number;
+
+  operaciones = [
+    {valor: 'Suma', muestraValor: 'Sumar'}, 
+    {valor: 'Resta', muestraValor: 'Restar'}, 
+    {valor: 'Multiplicación', muestraValor: 'Multiplicar'}, 
+    {valor: 'División', muestraValor: 'Dividir'}
+  ];
+
+  operacionSeleccionada: string = this.operaciones[0].valor;
+
+  operar() {
+    switch(this.operacionSeleccionada) {
+      case 'Suma':
+        this.resultado = this.valor1 + this.valor2;
+        break;
+      case 'Resta':
+        this.resultado = this.valor1 - this.valor2;
+        break;
+      case 'Multiplicación':
+        this.resultado = this.valor1 * this.valor2;
+        break;
+      case 'División':
+        this.resultado = this.valor1 / this.valor2;
+        break;
+    }
+  }
 }

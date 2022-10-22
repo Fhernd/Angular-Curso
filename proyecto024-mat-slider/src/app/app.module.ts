e61291c73feac9a5f9e98c9cbdedc58f10cd79a6
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-22 14:50:19 -0500

    Agregados módulos de Angular Material

--- proyecto024-mat-slider/src/app/app.module.ts
+++ proyecto024-mat-slider/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
+
+import { MatSliderModule } from '@angular/material/slider';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    FormsModule, 
+    MatSliderModule 
   ],
   providers: [],
   bootstrap: [AppComponent]

cd1a93a50f5d35c2c1a35e15799407d5266ffd45
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-22 08:43:33 -0500

    Inclusión de módulos para formularios y controles Material

--- proyecto022-mat-checkbox/src/app/app.module.ts
+++ proyecto022-mat-checkbox/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    FormsModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatCheckboxModule 
   ],
   providers: [],
   bootstrap: [AppComponent]

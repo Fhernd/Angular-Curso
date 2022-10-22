b30c8e00e2fa55d1f119a083e46a81969d627443
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-22 09:01:52 -0500

    Lógica para reconocer cuál o cuáles operaciones efectuar

--- proyecto022-mat-checkbox/src/app/app.component.ts
+++ proyecto022-mat-checkbox/src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto022-mat-checkbox';
+  valor1!: number;
+  valor2!: number;
+  resultado!: string;
+  opcionSuma = false;
+  opcionResta = false;
+  opcionMultiplicacion = false;
+  opcionDivision = false;
+
+  operar() {
+    this.resultado = '';
+
+    if (this.opcionSuma) {
+      const suma = this.valor1 + this.valor2;
+      this.resultado += `La suma es ${suma} `;
+    }
+    if (this.opcionResta) {
+      const resta = this.valor1 - this.valor2;
+      this.resultado += `La resta es ${resta} `;
+    }
+    if (this.opcionMultiplicacion) {
+      const multiplicacion = this.valor1 * this.valor2;
+      this.resultado += `La multiplicación es ${multiplicacion} `;
+    }
+    if (this.opcionDivision) {
+      const division = this.valor1 / this.valor2;
+      this.resultado += `La división es ${division} `;
+    }
+  }
 }

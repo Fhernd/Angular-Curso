108b739e589673bd73e46914a659829e0dff5dce
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-24 14:08:21 -0500

    Inclusión módulos para tablas de Angular Material

--- proyecto027-mat-table/src/app/app.module.ts
+++ proyecto027-mat-table/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+
+import { MatTableModule} from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, 
+    FormsModule, 
+    MatTableModule, 
+    MatInputModule, 
+    MatButtonModule 
   ],
   providers: [],
   bootstrap: [AppComponent]

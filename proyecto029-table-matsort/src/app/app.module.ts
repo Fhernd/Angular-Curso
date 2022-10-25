d83dca34a0b645013f63f0cfd31b469313af22d9
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-25 10:47:59 -0500

    Importación del módulo MatSortModule

--- proyecto029-table-matsort/src/app/app.module.ts
+++ proyecto029-table-matsort/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { MatTableModule} from '@angular/material/table';
+import { MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, 
+    MatTableModule, 
+    MatSortModule 
   ],
   providers: [],
   bootstrap: [AppComponent]

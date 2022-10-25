ac8ebf895a29b5d90bdc5500a23a5a34900e4dc6
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-25 10:22:56 -0500

    Clase para representar un artículo.

--- proyecto028-mat-paginator/src/app/app.component.ts
+++ proyecto028-mat-paginator/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto028-mat-paginator';
+  columnas: string[] = ['codigo', 'descripcion', 'precio'];
+
+  // datos: Articulo
 }

1677bcc618d73f0002d31712517974c79990d25d
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-25 15:10:49 -0500

    Implementación controlador para la componente

--- proyecto030-table-filtrar-datos/src/app/app.component.ts
+++ proyecto030-table-filtrar-datos/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Articulo } from './models/articulo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   columnas: string[] = ['codigo', 'descripcion', 'precio'];
 
-  
+  articulos: Articulo[] = [
+    new Articulo(1, 'papas', 100), 
+    new Articulo(2, 'manzanas', 200), 
+    new Articulo(3, 'naranjas', 250) 
+  ];
+  dataSource: any;
+
+  ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.articulos);
+  }
+
+  filtrar(event: Event) {
+    const filtro = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filtro.trim().toLowerCase;
+  }
 }

e2dab394b1a1bedb82d4e53bd068c6d95ed5eaf5
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-25 16:21:17 -0500

    Instanciación de un servicio de diálogos modales

--- proyecto031-mat-dialog/src/app/dialogs/articulo-dialogo/articulo-dialogo.component.ts
+++ proyecto031-mat-dialog/src/app/dialogs/articulo-dialogo/articulo-dialogo.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject} from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Articulo } from 'src/app/models/articulo';
 
 @Component({
   selector: 'app-articulo-dialogo',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticuloDialogoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<ArticuloDialogoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Articulo
+  ) { }
 
   ngOnInit(): void {
   }
 
+  cancelar() {
+    this.dialogRef.close();
+  }
 }

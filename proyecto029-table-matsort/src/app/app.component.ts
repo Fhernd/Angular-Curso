2b3538e6ccee0a6410a2672cbf219ff1bb7a2b3a
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-25 12:08:50 -0500

    Agregada la clase para representar un artículo

--- proyecto029-table-matsort/src/app/app.component.ts
+++ proyecto029-table-matsort/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto029-table-matsort';
+  columnas: string[] = ['codigo', 'descripcion', 'precio'];
+
+  // articulos: 
 }

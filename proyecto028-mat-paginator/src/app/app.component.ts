9105a54698103019436178d757afaa887e5172f6
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-25 10:28:10 -0500

    Lógica para la paginación en la componente

--- proyecto028-mat-paginator/src/app/app.component.ts
+++ proyecto028-mat-paginator/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Articulo } from './models/articulo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   columnas: string[] = ['codigo', 'descripcion', 'precio'];
 
-  // datos: Articulo
+  articulos: Articulo[] = [];
+  dataSource: any;
+
+  @ViewChild(MatPaginator, {static: true}) paginador!: MatPaginator;
+
+  ngOnInit(): void {
+    for (let x = 1; x <= 100; x++) {
+      this.articulos.push(new Articulo(x, `Artículo ${x}`, Math.trunc(Math.random() * 1000)));
+      this.dataSource = new MatTableDataSource<Articulo>(this.articulos);
+      this.dataSource.paginator = this.paginador;
+    }
+  }
 }

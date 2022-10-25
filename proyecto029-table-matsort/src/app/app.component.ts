aea20eade986d0ee890bcf67c86ec3bb86f80e9e
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-25 12:12:32 -0500

    Lógica para generar artículos aleatorios y crear un elemento de ordenamiento

--- proyecto029-table-matsort/src/app/app.component.ts
+++ proyecto029-table-matsort/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Articulo } from './models/articulo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   columnas: string[] = ['codigo', 'descripcion', 'precio'];
 
-  // articulos: 
+  articulos: Articulo[] = [];
+
+  @ViewChild(MatSort, {static: true}) sort!: MatSort;
+  dataSource: any;
+
+  ngOnInit(): void {
+    for (let x = 1; x <= 10; x++) {
+      this.articulos.push(new Articulo(x, `Artículo ${x}`, Math.trunc(Math.random() * 1000)));
+    }
+
+    this.dataSource = new MatTableDataSource<Articulo>(this.articulos);
+    this.dataSource.sort = this.sort;
+  }
 }

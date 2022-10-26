de7210845dacc915e1bff1a5a9e2832b704a3f52
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-26 10:15:36 -0500

    Vista HTML para el diálogo emergente

--- proyecto031-mat-dialog/src/app/app.module.ts
+++ proyecto031-mat-dialog/src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ArticuloDialogoComponent } from './dialogs/articulo-dialogo/articulo-dialogo.component';
 
+import { FormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +19,12 @@ import { ArticuloDialogoComponent } from './dialogs/articulo-dialogo/articulo-di
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    MatInputModule, 
+    FormsModule, 
+    MatDialogModule, 
+    MatButtonModule, 
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]

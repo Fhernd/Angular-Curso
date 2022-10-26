b3629f27abf35b097b42b0ec09ba10635d1be724
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-26 14:19:30 -0500

    Codificando la componente que representa un dado

--- proyecto032-mat-menu/src/app/app.module.ts
+++ proyecto032-mat-menu/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { JuegoDadosComponent } from './juego-dados/juego-dados.component';
+import { DadoComponent } from './dado/dado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JuegoDadosComponent
+    JuegoDadosComponent,
+    DadoComponent
   ],
   imports: [
     BrowserModule,

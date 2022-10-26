4b549db3410eb3b6f0566565c7f32b175f25bb58
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-26 14:56:40 -0500

    Incorporación de módulos para menú

--- proyecto033-mat-menu-submenus/src/app/app.module.ts
+++ proyecto033-mat-menu-submenus/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]

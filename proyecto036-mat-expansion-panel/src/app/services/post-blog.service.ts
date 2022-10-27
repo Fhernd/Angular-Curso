db12208dee304db0bf2db7a2f8c8ca65baacdd15
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-27 16:12:42 -0500

    Solicitud GET para obtener datos desde la API

--- proyecto036-mat-expansion-panel/src/app/services/post-blog.service.ts
+++ proyecto036-mat-expansion-panel/src/app/services/post-blog.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class PostBlogService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  retornar() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
+  }
 }

5aa44253587e744782e50447c0e752edb4a02507
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-27 13:23:11 -0500

    Método para obtener (recargar) otro usuario.

--- proyecto034-mat-card/src/app/app.component.ts
+++ proyecto034-mat-card/src/app/app.component.ts
@@ -21,4 +21,8 @@ export class AppComponent implements OnInit {
     this.usuarioService.obtener()
       .subscribe(resultado => this.usuario = resultado);
   }
+
+  recargar() {
+    this.recuperarPersona();
+  }
 }

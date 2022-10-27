c14db7098a7b847cf948d3c3c0040ac57348a574
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-27 14:34:46 -0500

    Inclusión de MatToolbarModule

--- proyecto035-mat-toolbar/src/app/app.module.ts
+++ proyecto035-mat-toolbar/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

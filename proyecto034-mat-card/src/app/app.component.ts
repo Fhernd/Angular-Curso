60fc56b8ad5f0bff74f184c3a19bb1d70cb4d7c9
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-27 13:21:29 -0500

    Método para recuperar los datos de un usuario.

--- proyecto034-mat-card/src/app/app.component.ts
+++ proyecto034-mat-card/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from './services/usuario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto034-mat-card';
+export class AppComponent implements OnInit {
+  usuario: any;
+
+  constructor(private usuarioService: UsuarioService) {
+
+  }
+
+  ngOnInit(): void {
+      this.recuperarPersona();
+  }
+
+  recuperarPersona() {
+    this.usuarioService.obtener()
+      .subscribe(resultado => this.usuario = resultado);
+  }
 }

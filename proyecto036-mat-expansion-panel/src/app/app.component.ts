f50314cf2626c336b5479d9153decfa4bcd98595
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-28 10:01:16 -0500

    Lógica para traer datos del servidor

--- proyecto036-mat-expansion-panel/src/app/app.component.ts
+++ proyecto036-mat-expansion-panel/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostBlogService } from './services/post-blog.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto036-mat-expansion-panel';
+export class AppComponent implements OnInit {
+  post: any;
+
+  constructor(private postBlogServie: PostBlogService) {
+
+  }
+
+  ngOnInit(): void {
+      this.postBlogServie.retornar().subscribe(resultado => this.post = resultado);
+  }
 }

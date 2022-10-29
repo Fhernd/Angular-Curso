7afefab9aab6e11c2d4dfc7e91b902acfd2ea9d0
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-29 16:46:49 -0500

    Inicialización de la componente para barra de menú

--- proyecto039-bootstrap-componente/src/app/app.module.ts
+++ proyecto039-bootstrap-componente/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BarraMenuComponent } from './componentes/barra-menu/barra-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ proyecto039-bootstrap-componente/src/app/componentes/barra-menu/barra-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-barra-menu',
+  templateUrl: './barra-menu.component.html',
+  styleUrls: ['./barra-menu.component.css']
+})
+export class BarraMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

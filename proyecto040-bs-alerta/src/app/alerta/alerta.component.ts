302f0136827b7a3176d7e02c94803460994a5a89
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-31 14:46:56 -0500

    Atributos de entrada para la componente Alerta

--- proyecto040-bs-alerta/src/app/alerta/alerta.component.ts
+++ proyecto040-bs-alerta/src/app/alerta/alerta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bs-alerta',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alerta.component.css']
 })
 export class AlertaComponent implements OnInit {
+  @Input()
+  color!: string;
+  @Input()
+  mensaje!: string;
 
   constructor() { }
 

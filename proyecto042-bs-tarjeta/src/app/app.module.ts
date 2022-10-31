98c8ba5430fef77d4cf18025f2fa64a8a8797496
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-31 16:48:03 -0500

    Inicialización de la componente Tarjeta

--- proyecto042-bs-tarjeta/src/app/app.module.ts
+++ proyecto042-bs-tarjeta/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetaComponent
   ],
   imports: [
     BrowserModule,

13d707faa6cb290f81aeb4abfddd25225c5d8bdd
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-31 15:27:28 -0500

    Lógica de la componente principal

--- proyecto041-bs-tabla/src/app/app.component.ts
+++ proyecto041-bs-tabla/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostBlogService } from './services/post-blog.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto41-bs-tabla';
+export class AppComponent implements OnInit {
+  posts: any;
+  titulosColumnas = ['Código usuario', 'Código de post', 'Título', 'Descripción'];
+
+  constructor(private postBlogService: PostBlogService) {
+
+  }
+
+  ngOnInit() {
+    this.postBlogService.obtenerPosts().subscribe((resultado) => {
+      this.posts = resultado;
+    });
+  }
 }

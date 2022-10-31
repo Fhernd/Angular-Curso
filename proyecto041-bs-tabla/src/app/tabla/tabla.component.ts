274ebf2882b1d1fc5b30a51c0d79d4a738071eff
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-31 15:15:34 -0500

    Inicialización de la componente Tabla

---
+++ proyecto041-bs-tabla/src/app/tabla/tabla.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bs-tabla',
+  templateUrl: './tabla.component.html',
+  styleUrls: ['./tabla.component.css']
+})
+export class TablaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

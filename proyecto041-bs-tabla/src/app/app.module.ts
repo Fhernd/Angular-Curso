6be22261987ef59c945f9bae2ec1f68842e377d9
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-10-31 15:13:00 -0500

    Servicio que recupera datos desde un servidor externo

--- proyecto041-bs-tabla/src/app/app.module.ts
+++ proyecto041-bs-tabla/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

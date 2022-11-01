6054e383f59d529ed19338439fb42f2a1199aa72
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-01 16:26:27 -0500

    Inclusión módulo NgbModule

--- proyecto043-ng-bootstrap/src/app/app.module.ts
+++ proyecto043-ng-bootstrap/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

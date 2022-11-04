86812b47fd4965a5044af950259cc98a49954431
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-04 15:59:44 -0500

    Variables para las registrar las operaciones aritméticas

--- proyecto045-ng-switch/src/app/app.component.ts
+++ proyecto045-ng-switch/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto045-ng-switch';
+  valor1!: number;
+  valor2!: number;
+  operacion: string = 'Ninguna';
+  resultado!: number;
 }

5802df38105d424901ffc85bc2d081773e5058d4
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-05 16:11:51 -0500

    Lógica para aplicar estilo a un elemento HTML desde una directiva

--- proyecto046-directiva-propia/src/app/resaltado.directive.ts
+++ proyecto046-directiva-propia/src/app/resaltado.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appResaltado]'
 })
 export class ResaltadoDirective {
 
-  constructor() { }
+  constructor(private elemento: ElementRef) { 
+    elemento.nativeElement.style.backgroundColor = 'yellow';
+  }
 
 }

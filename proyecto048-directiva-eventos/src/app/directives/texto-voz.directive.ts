038a00e1a5122ab8cae4931794690880e9d71fd5
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-06 12:40:50 -0500

    Implementación lógica de la directiva de texto a voz

--- proyecto048-directiva-eventos/src/app/directives/texto-voz.directive.ts
+++ proyecto048-directiva-eventos/src/app/directives/texto-voz.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[textoVoz]'
 })
 export class TextoVozDirective {
 
-  constructor() { }
+  constructor(private elemento: ElementRef) { }
 
+  @HostListener('mouseenter')
+  entradaMouse() {
+    speechSynthesis.speak(new SpeechSynthesisUtterance(this.elemento.nativeElement.textContent));
+  }
+
+  @HostListener('mouseleave')
+  salidaMouse() {
+    speechSynthesis.cancel();
+  }
 }

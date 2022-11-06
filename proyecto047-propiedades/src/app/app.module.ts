a1cfb70391ce6900dd1d42ccad384c60b668264a
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-06 09:18:25 -0500

    Inclusión de directive en el módulo de la aplicación

--- proyecto047-propiedades/src/app/app.module.ts
+++ proyecto047-propiedades/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltadoDirective
   ],
   imports: [
     BrowserModule

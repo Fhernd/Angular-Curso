da0ce8955ef29358fd8af18fe76700acc4a5a956
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-11 16:43:48 -0500

    Componente para el continente de América/Argentina

--- proyecto053-rutas-anidadas/src/app/app.module.ts
+++ proyecto053-rutas-anidadas/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AmericaComponent } from './components/america/america.component';
 import { ColombiaComponent } from './components/america/colombia/colombia.component';
 import { MexicoComponent } from './components/america/mexico/mexico.component';
+import { ArgentinaComponent } from './components/america/argentina/argentina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AmericaComponent,
     ColombiaComponent,
-    MexicoComponent
+    MexicoComponent,
+    ArgentinaComponent
   ],
   imports: [
     BrowserModule,

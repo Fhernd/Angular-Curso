42faeeab12ac0e08bca15760f4527c46658c5594
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-11 15:46:06 -0500

    Agregación de rutas

--- proyecto052-ruta-no-existente/src/app/app-routing.module.ts
+++ proyecto052-ruta-no-existente/src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { Pagina404Component } from './components/pagina404/pagina404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent
+  },
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: 'mapa',
+    component: MapaComponent
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent
+  },
+  {
+    path: 'acerca-de',
+    component: AcercaDeComponent
+  },
+  {
+    path: 'pagina-404',
+    component: Pagina404Component
+  },
+  {
+    path: '**',
+    redirectTo: 'pagina-404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

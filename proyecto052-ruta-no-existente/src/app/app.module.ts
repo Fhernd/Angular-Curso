6e4a98b6c35685a1681e5e7e10a1370e48e638cb
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-11 15:42:22 -0500

    Estructura inicial para la componente Acerca De

--- proyecto052-ruta-no-existente/src/app/app.module.ts
+++ proyecto052-ruta-no-existente/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { MapaComponent } from './components/mapa/mapa.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     MapaComponent,
-    ContactoComponent
+    ContactoComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,

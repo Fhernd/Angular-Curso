ddfb8e3741272ec1563809f8450dda21b663dc2b
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-11 14:32:01 -0500

    Creada la estructura de la componente de tabla de multiplicar

--- proyecto051-routing-params/src/app/app.module.ts
+++ proyecto051-routing-params/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TablaMultiplicarComponent } from './components/tabla-multiplicar/tabla-multiplicar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaMultiplicarComponent
   ],
   imports: [
     BrowserModule,
---
+++ proyecto051-routing-params/src/app/components/tabla-multiplicar/tabla-multiplicar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-multiplicar',
+  templateUrl: './tabla-multiplicar.component.html',
+  styleUrls: ['./tabla-multiplicar.component.css']
+})
+export class TablaMultiplicarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

b88d2dd9ead5e75dd401c86e7ce4ca52b43a038b
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-11 14:38:50 -0500

    Lógica para leer un parámetro de ruta en una componente

--- proyecto051-routing-params/src/app/components/tabla-multiplicar/tabla-multiplicar.component.ts
+++ proyecto051-routing-params/src/app/components/tabla-multiplicar/tabla-multiplicar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-tabla-multiplicar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaMultiplicarComponent implements OnInit {
 
-  constructor() { }
+  numero!: number;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((parametros: ParamMap) => {
+      this.numero = parseInt(parametros.get('numero')!);
+    });
   }
 
 }

c19584bc8a1f85327c8f02f432b2b51866581726
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-11 14:33:03 -0500

    Creada la ruta de tabla de multiplicar con un parámetro de ruta

--- proyecto051-routing-params/src/app/app-routing.module.ts
+++ proyecto051-routing-params/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TablaMultiplicarComponent } from './components/tabla-multiplicar/tabla-multiplicar.component';
 
 const routes: Routes = [{
-  
+  path: 'tabla/:numero',
+  component: TablaMultiplicarComponent
 }];
 
 @NgModule({

0f21ca0910c87c5340c3b12366832c85563f63da
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 08:49:09 -0500

    Componente para el país de España

--- proyecto054-lazy-loading/src/app/modules/europa/europa.module.ts
+++ proyecto054-lazy-loading/src/app/modules/europa/europa.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EuropaRoutingModule } from './europa-routing.module';
+import { EspanaComponent } from './europa/espana/espana.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EspanaComponent
+  ],
   imports: [
     CommonModule,
     EuropaRoutingModule

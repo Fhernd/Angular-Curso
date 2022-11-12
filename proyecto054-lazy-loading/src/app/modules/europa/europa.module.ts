5b70e0d1e25f9244586032112a2e371fc33e1a1e
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 08:50:58 -0500

    Componente para el país de Italia

--- proyecto054-lazy-loading/src/app/modules/europa/europa.module.ts
+++ proyecto054-lazy-loading/src/app/modules/europa/europa.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { EuropaRoutingModule } from './europa-routing.module';
 import { EspanaComponent } from './europa/espana/espana.component';
 import { AlemaniaComponent } from './europa/alemania/alemania.component';
+import { ItaliaComponent } from './europa/italia/italia.component';
 
 
 @NgModule({
   declarations: [
     EspanaComponent,
-    AlemaniaComponent
+    AlemaniaComponent,
+    ItaliaComponent
   ],
   imports: [
     CommonModule,

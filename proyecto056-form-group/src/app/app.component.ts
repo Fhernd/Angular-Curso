bcc182b75f14ef958ccd397e35ea3e26e0f71555
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 12:27:09 -0500

    Creado objeto formGroup para agrupar controles del formulario de contacto

--- proyecto056-form-group/src/app/app.component.ts
+++ proyecto056-form-group/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto056-form-group';
+  datos!: string;
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl(''),
+    mail: new FormControl(''),
+    mensaje: new FormControl()
+  });
+
+  onSubmit() {
+    this.datos = `
+      Nombre: ${this.formularioContacto.value.nombre}
+      Email: ${this.formularioContacto.value.mail}
+      Mensaje: ${this.formularioContacto.value.mensaje}
+    `;
+  }
 }

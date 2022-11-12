e92562ddbc108a89aeade01d43aa1882e72723f5
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 09:00:39 -0500

    Rutas con carga diferida (perezosa) en la ruta principal

--- proyecto054-lazy-loading/src/app/app-routing.module.ts
+++ proyecto054-lazy-loading/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AmericaComponent } from './modules/america/america/america.component';
+import { EuropaComponent } from './modules/europa/europa/europa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'america',
+    component: AmericaComponent,
+    loadChildren: () => import('./modules/america/america.module').then(m => m.AmericaModule)
+  },
+  {
+    path: 'europa',
+    component: EuropaComponent,
+    loadChildren: () => import('./modules/europa/europa.module').then(m => m.EuropaModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

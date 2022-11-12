75fe19a8bf68ea4b460bbb916f234a654115e885
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 08:40:15 -0500

    Módulos para América y Europa

---
+++ proyecto054-lazy-loading/src/app/modules/america/america.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AmericaRoutingModule } from './america-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AmericaRoutingModule
+  ]
+})
+export class AmericaModule { }
---
+++ proyecto054-lazy-loading/src/app/modules/europa/europa.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EuropaRoutingModule } from './europa-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EuropaRoutingModule
+  ]
+})
+export class EuropaModule { }

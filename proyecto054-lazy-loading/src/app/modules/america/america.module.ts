48d420f0ff037af28889e6bce360465bcb4e91f8
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 08:44:48 -0500

    Componente para el país de México

--- proyecto054-lazy-loading/src/app/modules/america/america.module.ts
+++ proyecto054-lazy-loading/src/app/modules/america/america.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AmericaRoutingModule } from './america-routing.module';
 import { ColombiaComponent } from './america/colombia/colombia.component';
 import { ArgentinaComponent } from './america/argentina/argentina.component';
+import { MexicoComponent } from './america/mexico/mexico.component';
 
 
 @NgModule({
   declarations: [
     ColombiaComponent,
-    ArgentinaComponent
+    ArgentinaComponent,
+    MexicoComponent
   ],
   imports: [
     CommonModule,

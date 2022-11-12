8b1130db38eaaf0715febb88a28169f72a715f89
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 15:04:40 -0500

    Incorporación del módulo de formularios reactivos

--- proyecto057-controles-seleccion/src/app/app.module.ts
+++ proyecto057-controles-seleccion/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

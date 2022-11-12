052384f9156112d0f997beb7063635122384ab85
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 09:15:30 -0500

    Rutas para el continente de Europa

--- proyecto054-lazy-loading/src/app/modules/europa/europa-routing.module.ts
+++ proyecto054-lazy-loading/src/app/modules/europa/europa-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlemaniaComponent } from './europa/alemania/alemania.component';
+import { EspanaComponent } from './europa/espana/espana.component';
+import { ItaliaComponent } from './europa/italia/italia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'espana',
+    component: EspanaComponent
+  },
+  {
+    path: 'alemania',
+    component: AlemaniaComponent
+  },
+  {
+    path: 'italia',
+    component: ItaliaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

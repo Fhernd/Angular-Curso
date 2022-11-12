2f71bfb6ff7f14e46fac7d3293e32e2a8f6881c3
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 09:14:25 -0500

    Rutas para el continente de América

--- proyecto054-lazy-loading/src/app/modules/america/america-routing.module.ts
+++ proyecto054-lazy-loading/src/app/modules/america/america-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArgentinaComponent } from './america/argentina/argentina.component';
+import { ColombiaComponent } from './america/colombia/colombia.component';
+import { MexicoComponent } from './america/mexico/mexico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'colombia',
+    component: ColombiaComponent
+  },
+  {
+    path: 'argentina',
+    component: ArgentinaComponent
+  },
+  {
+    path: 'mexico',
+    component: MexicoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

79ead996872d73bc548d57824ad508f0e53b92e6
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-12 11:37:35 -0500

    Inicialización de la componente principal

--- proyecto055-form-reactivos/src/app/app.component.ts
+++ proyecto055-form-reactivos/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto055-form-reactivos';
+export class AppComponent implements OnInit {
+  actividad = new FormControl();
+
+  lista!: string[];
+
+  ngOnInit() : void {
+    this.lista = [];
+
+    const datos = localStorage.getItem('actividades');
+
+    if (datos != null) {
+      let arreglo = JSON.parse(datos);
+
+      if (arreglo != null) {
+        for (const actividad of arreglo) {
+          this.lista.push(actividad);
+        }
+      }
+    }
+  }
 }

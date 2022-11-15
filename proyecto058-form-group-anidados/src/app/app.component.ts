7c7571bfe17792237e4250639a58b80538e5e829
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-15 14:50:55 -0500

    Lógica para el FormGroup anidado

--- proyecto058-form-group-anidados/src/app/app.component.ts
+++ proyecto058-form-group-anidados/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto058-form-group-anidados';
+  resultado!: string;
+
+  formularioAlumno = new FormGroup({
+    dni: new FormControl(''),
+    nombre: new FormControl(''),
+    notas: new FormGroup({
+      nota1: new FormControl(),
+      nota2: new FormControl(),
+      nota3: new FormControl()
+    })
+  });
+
+  onSubmit() {
+    const nota1 = parseInt(this.formularioAlumno.value.notas?.nota1);
+    const nota2 = parseInt(this.formularioAlumno.value.notas?.nota1);
+    const nota3 = parseInt(this.formularioAlumno.value.notas?.nota1);
+
+    if (nota1 >= 4 && nota2 >= 4 && nota3 >= 4) {
+      this.resultado = 'El alumno queda aprobado por esas notas';
+    } else {
+      this.resultado = 'El alumno no paureba por esas notas';
+    }
+  }
 }

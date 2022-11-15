1545bf5c86838e34b29997697152a6df88c88a6b
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-15 15:16:11 -0500

    Lógica para validaciones de los campos del formulario de contacto

--- proyecto059-validaciones/src/app/app.component.ts
+++ proyecto059-validaciones/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto059-validaciones';
+  resultado!: string;
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    mensaje: new FormControl('', [Validators.required, Validators.maxLength(500)])
+  });
+
+  onSubmit() {
+    if (this.formularioContacto.valid) {
+      this.resultado = 'Todos los datos son válidos.';
+    } else {
+      this.resultado = 'Hay datos inválidos en el formulario.';
+    }
+  }
 }

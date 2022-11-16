de6b82b4f9b639ac711258edac87bbe79f9e8452
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2022-11-16 11:01:19 -0500

    Formulario creado con la clase FormBuilder

--- proyecto061-form-builder/src/app/app.component.ts
+++ proyecto061-form-builder/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto061-form-builder';
+  resultado!: string;
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  formularioContacto = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(10)]],
+    mail: ['', [Validators.required, Validators.email]],
+    mensaje: ['', [Validators.required, Validators.maxLength(500)]]
+  });
+
+  onSubmit() {
+    if (this.formularioContacto.valid) {
+      this.resultado = 'Todos los datos son válidos.';
+    } else {
+      this.resultado = 'Hay datos inválidos en el formulario.';
+    }
+  }
 }
